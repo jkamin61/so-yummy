4f6ecfa6ff0e8bce4dcd06024f3c2b1bf04c32d6
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-11-06 22:37:46 +0100

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/app/components/header";
+import About from "@/app/components/about";
 
 export default function Home() {
     return (
         <>
             <Header/>
+            <About/>
         </>
     )
 }
