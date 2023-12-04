4231ea70237b2d0c4a668e3eace426a78e62984e
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-12-04 15:04:39 +0100

    import navigation obj

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,22 +4,9 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../public/header-logo.jpg";
 import avatar from "../../public/avatar-ex.png";
-import spinach from "../../public/spinachi.png"
 import Switch from '@mui/material/Switch';
 import Link from "next/link";
-
-type Navigation = {
-    name: string,
-    href: string
-}
-
-const navigation: Navigation[] = [
-    {name: 'Categories', href: '#'},
-    {name: 'Add recipes', href: '#'},
-    {name: 'My recipes', href: '#'},
-    {name: 'Favorites', href: '#'},
-    {name: 'Shopping list', href: '#'},
-]
+import {navigation} from "@/app/components/partials";
 
 const label = {inputProps: {'aria-label': 'Switch demo'}};
 
@@ -27,9 +14,9 @@ const Header: React.FC = () => {
     const [loggedIn, setLoggedIn] = React.useState<boolean>(false)
     const [mobileMenu, setMobileMenu] = React.useState<boolean>(false)
     return (
-        <header>
+        <>
             {!mobileMenu &&
-                <div className="flex items-center p-4 justify-between">
+                <div className="flex items-center p-4 justify-between ">
                     <Link
                         className="rounded-xl w-10 h-10 bg-asparagus flex items-center justify-center hover:animate-pulse"
                         href="/">
@@ -64,7 +51,7 @@ const Header: React.FC = () => {
                             href="/">
                             <Image src={logo} className="w-7 h-7" alt="logo"/>
                         </Link>
-                        <button  onClick={() => setMobileMenu(false)}>
+                        <button onClick={() => setMobileMenu(false)}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"
                                  fill="none">
                                 <path d="M22.6673 9.33334L9.33398 22.6667M9.33398 9.33334L22.6673 22.6667"
@@ -77,13 +64,16 @@ const Header: React.FC = () => {
                         <ul className="flex flex-col gap-8 p-14 pb-0 justify-center items-center">
                             {navigation.map((item, index) => (
                                 <li key={index}>
-                                    <Link href={item.href} className="text-xl hover:text-asparagus motion-safe:hover:text-asparagus">{item.name}</Link>
+                                    <Link href={item.href}
+                                          className="text-xl hover:text-asparagus motion-safe:hover:text-asparagus">{item.name}</Link>
                                 </li>
                             ))}
                         </ul>
                         <div className="flex gap-2 justify-center items-center mt-8">
-                            <svg className="hover:animate-bounce motion-reduce:animate-bounce" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"
-                                 fill="none">
+                            <svg
+                                className="hover:animate-bounce motion-reduce:animate-bounce hover:cursor-pointer"
+                                xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"
+                                fill="none">
                                 <path
                                     d="M9.16666 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16666C15.8333 5.48477 12.8486 2.5 9.16666 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16666C2.5 12.8486 5.48477 15.8333 9.16666 15.8333Z"
                                     stroke="#22252A" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
@@ -91,15 +81,15 @@ const Header: React.FC = () => {
                                       strokeLinecap="round"
                                       strokeLinejoin="round"/>
                             </svg>
-                            <button className="text-xl hover:text-asparagus motion-safe:hover:text-asparagus">Search</button>
+                            <button className="text-xl hover:text-asparagus motion-safe:hover:text-asparagus">Search
+                            </button>
                         </div>
                     </nav>
                     <div className="flex justify-between w-full p-2 items-end">
                         <Switch {...label}/>
-                        {/*<Image src={spinach} alt="spinachi" className="w-80 h-80 rotate-65 relative left-12 top-10 blur-xs"/>*/}
                     </div>
                 </div>}
-        </header>
+        </>
     );
 }
 
