b9e3b07e89f8d497630bfb23d889df7d8e6ec709
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-12-04 15:04:16 +0100

    added components to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import Header from "@/app/components/header";
-import About from "@/app/components/about";
+import About from "@/app/components/main-page/about";
+import Footer from "@/app/components/footer";
+import ChooseYourBreakfast from "@/app/components/main-page/choose-your-breakfast";
+import Search from "@/app/components/main-page/search";
+import PreviewCategories from "@/app/components/main-page/preview-categories";
 
-export default function Home() {
+export default function Home(): JSX.Element {
     return (
         <>
-            <Header/>
-            <About/>
+            <header>
+                <Header/>
+            </header>
+            <main>
+                <About/>
+                <ChooseYourBreakfast/>
+                <Search/>
+                <PreviewCategories/>
+            </main>
+            <footer>
+                <Footer/>
+            </footer>
         </>
     )
 }
