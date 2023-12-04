ed874ed597fae8ae97a3433dd18d2085cb4c1155
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-12-04 15:03:50 +0100

    navi object

---
+++ app/components/partials.ts
@@ -0,0 +1,12 @@
+type Navigation = {
+    name: string,
+    href: string
+}
+
+export const navigation: Navigation[] = [
+    {name: 'Categories', href: '#'},
+    {name: 'Add recipes', href: '#'},
+    {name: 'My recipes', href: '#'},
+    {name: 'Favorites', href: '#'},
+    {name: 'Shopping list', href: '#'},
+]
