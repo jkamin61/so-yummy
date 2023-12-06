5131394a5aae1dab223759117e300b158182d464
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-12-06 15:52:19 +0100

    breakfast mobile ver.

--- app/components/main-page/choose-your-breakfast.tsx
+++ app/components/main-page/choose-your-breakfast.tsx
@@ -1,6 +1,21 @@
+import Image from "next/image";
+import pancakes from "../../../public/banana-pancakes.png";
+
 function ChooseYourBreakfast(): JSX.Element {
     return (
-        <div></div>
+        <section className="container p-4">
+            <h2 className="text-2xl font-bold mb-6">Breakfast</h2>
+            <div className="relative flex justify-center">
+                <Image className="" src={pancakes} alt="banana pancakes"/>
+                <div
+                    className="absolute bottom-4 text-charcoal bg-white h-12 w-72 flex justify-center items-center rounded shadow">Banana
+                    Pancakes
+                </div>
+            </div>
+            <div className="flex justify-end my-4">
+                <button className="h-10 w-24 text-white text-sm font-extralight bg-asparagus rounded">See all</button>
+            </div>
+        </section>
     );
 }
 
