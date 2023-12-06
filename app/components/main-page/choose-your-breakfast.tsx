dbdbf8199a941a9b218143e5db2f36bab93fd6e4
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-12-06 16:56:00 +0100

    replaced content with card component

--- app/components/main-page/choose-your-breakfast.tsx
+++ app/components/main-page/choose-your-breakfast.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
 import pancakes from "../../../public/banana-pancakes.png";
+import Card from "../card";
+import {StaticImageData} from "next/image";
+
+const breakfasts: { name: string, image: StaticImageData, title: string } = {
+    name: "Banana Pancakes",
+    image: pancakes,
+    title: "Breakfast"
+}
 
 function ChooseYourBreakfast(): JSX.Element {
     return (
         <section className="container p-4">
-            <h2 className="text-2xl font-bold mb-6">Breakfast</h2>
-            <div className="relative flex justify-center">
-                <Image className="" src={pancakes} alt="banana pancakes"/>
-                <div
-                    className="absolute bottom-4 text-charcoal bg-white h-12 w-72 flex justify-center items-center rounded shadow">Banana
-                    Pancakes
-                </div>
-            </div>
+            <Card image={breakfasts.image} name={breakfasts.name} title={breakfasts.title}/>
             <div className="flex justify-end my-4">
                 <button className="h-10 w-24 text-white text-sm font-extralight bg-asparagus rounded">See all</button>
             </div>
